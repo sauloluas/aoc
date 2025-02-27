b1bdc9a1575d39fb28dfde285a9252f3ba509e95
sauloluas <sauloroba@gmail.com>
2025-02-27 10:49:37 -0300

    polishing for part two

--- Y23/d1-trebuchet/src/main.rs
+++ Y23/d1-trebuchet/src/main.rs
@@ -1,13 +1,13 @@
 use std::fs::read_to_string;
 
-fn first_numeric(line: &str) -> Option<char> {
+fn first_number(line: &str) -> Option<u32> {
 
     let mut digits = line.chars();
 
     while let Some(digit) = digits.next() {
 
         if digit.is_numeric() {
-            return Some(digit);
+            return digit.to_digit(10);
         }
 
     };
@@ -16,14 +16,14 @@ fn first_numeric(line: &str) -> Option<char> {
 
 }
 
-fn last_numeric(line: &str) -> Option<char> {
+fn last_number(line: &str) -> Option<u32> {
 
     let mut digits = line.chars().rev();
 
     while let Some(digit) = digits.next() {
 
         if digit.is_numeric() {
-            return Some(digit);
+            return digit.to_digit(10);
         }
 
     }
@@ -42,11 +42,10 @@ fn main() {
 
     let mut sum: u32 = 0;
 
-
     while let Some(line) = input.next() {
         
-        let first = first_numeric(&line).unwrap().to_digit(10).unwrap();
-        let last = last_numeric(&line).unwrap().to_digit(10).unwrap();
+        let first = first_number(&line).unwrap();
+        let last = last_number(&line).unwrap();
 
         let cal_value = 10*first + last;
 
